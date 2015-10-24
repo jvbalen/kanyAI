6fc523cde03d6237e8562d1b4842a03eb584fbc8
hvkoops <hvkoops@gmail.com>
2015-10-24 14:00:46 +0200

    added parsing of the second sheet of excel database

--- database/read_db.py
+++ database/read_db.py
@@ -2,10 +2,31 @@
 
 import numpy as np
 
-f = np.genfromtxt("sample_database.csv", delimiter=";", dtype=object)
+# First sheet in the DB
+track_db = np.genfromtxt("sample_database.csv", delimiter=";", dtype=object)
+# Second sheet in the DB
+sample_db = np.genfromtxt("sample_database2.csv", delimiter=";", dtype=object)
+# List of audio files in the dataset
+file_db = np.genfromtxt("audio_files.csv", dtype=object)
 
-trackids = f[:,0]
-artists = f[:,1]
-titles = f[:,2]
-years = f[:,3]
-genres = f[:,4]
+# contents of the first sheet in sample_database.xls
+tracks_trackids = track_db[:,0]
+tracks_artists = track_db[:,1]
+tracks_titles = track_db[:,2]
+tracks_years = track_db[:,3].astype(int)
+tracks_genres = track_db[:,4]
+
+# contents of the second sheet in sample_database.xls
+sample_sampleids = sample_db[:,0]
+sample_sampling_track = sample_db[:,1]
+sample_track_sampled = sample_db[:,2]
+sample_interpolation = sample_db[:,3]
+sample_time_sampled =  sample_db[:,4]
+sample_times_of_sample = sample_db[:,5]
+sample_repetitions = sample_db[:,6]
+sample_type_loop = sample_db[:,7]
+sample_comments = sample_db[:,8]
+
+# for i in np.arange(0,len(trackids)):
+# 	if not trackids[i] in file_db:
+# 		clean_db = np.delete(track_db, (i), axis=0)
