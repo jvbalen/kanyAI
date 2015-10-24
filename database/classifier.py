f46f0306028d5f6362aaf26ceacd1ed0e369ec9e
hvkoops <hvkoops@gmail.com>
2015-10-24 15:37:21 +0200

    classifier update, pipeline with random features

--- database/classifier.py
+++ database/classifier.py
@@ -10,40 +10,40 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 from sklearn.svm import LinearSVC
-from sklearn.preprocessing import normalize
-from sklearn.preprocessing import Imputer
-from sklearn import preprocessing
-from sklearn import svm
-from sklearn.linear_model import SGDClassifier
-from sklearn import tree
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.decomposition import PCA, KernelPCA
-import wave
-import minirank
 
 import numpy as np
-import math
-import time
-import glob
-import sys
-import pandas as pd
-import ast
 
 # read the feature csv file
 featurefile = "features.csv"
+size = 300
+nfeats = 17
 # features = np.genfromtxt(featurefile, delimiter=",")
-features = np.array([[0,1,0],[1,0,1],[1,1,1],[0,0,1],[0,0,1]])
+features = np.random.rand(size,nfeats)
+
 classfile = "classes.csv"
 # classes = np.genfromtxt(classfile, delimiter=",")
-classes = np.array([0,1,0,1,1])
+classes = np.random.randint(2, size=size)
 test_size = 0.3
-random_state = 32
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, classes, test_size=test_size, random_state=random_state)
 
 # ================================= MACHINE LEARNING ============================================ 
-clf = Pipeline([('classification', GradientBoostingClassifier(n_estimators=500, learning_rate=0.1, max_depth=3, random_state=0, verbose=1))])
-print "Fitting model..."
-clf.fit(X_train, y_train)
-print "Testing score..."
-scores = clf.score(X_test, y_test)
-print "Accuracy: " + str(scores)
+clf_rand_state = np.random.randint(100, size=1)[0]
+clf = Pipeline([
+	('classification', GradientBoostingClassifier(n_estimators=500, learning_rate=0.1, max_depth=3, random_state=clf_rand_state))
+	])
+
+# k repeats
+k = 10
+scores = np.zeros(k)
+for i in np.arange(k):
+	random_state = np.random.randint(100, size=1)[0]
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, classes, test_size=test_size, random_state=random_state)
+	print "Fitting model: " + str(i)
+	clf.fit(X_train, y_train)
+	print "Testing score..."
+	score = clf.score(X_test, y_test)
+	print "Accuracy: " + str(score)
+	scores[i] = score
+
+avg_acc = np.mean(scores)
+print "Scores: " + str(scores)
+print "Average accuracy: " + str(avg_acc)
