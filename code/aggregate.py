6eebc9386968b8fc7718e9e8fdda708cd749cb08
nglazyrin <nglazyrin@gmail.com>
2015-10-25 15:28:24 +0500

    The code for feature aggregation, code comments and cleanup, aggregated constant-Q features.

--- code/aggregate.py
+++ code/aggregate.py
@@ -2,17 +2,42 @@ __author__ = 'Jan'
 
 import numpy as np
 import librosa as lr
+import os
 
 sr = 44100
 hop_size = 1024
-beat_folder = '../data/aubiotempo/'
+beat_file_suffix = '_beats.csv'
+default_beats_folder = '../data/frames/'
+default_features_folder = '../data/constantq/'
 
-def beat_aggregation(feature_data, file_name, time_col=False, agg_function=np.median):
-    feat_frames = np.genfromtxt(feature_data).T
-    if time_col:
-        feat_frames = feat_frames[1:]
-    beat_times = np.genfromtxt(beat_folder + file_name + '_vamp_vamp-aubio_aubiotempo_beats.csv')
-    beat_frames = beat_times * float(sr)/hop_size
 
-    feat_beats = lr.feature.sync(feat_frames, beat_frames, aggregate=agg_function)
-    np.savetxt(feature_data.split('.')[-2] + '_beat.csv', feat_beats.T, delimiter=',')
+def aggregate_vamp_features(features_folder, beats_folder, time_col=True, agg_function=np.median, output_folder=None):
+    '''
+    Aggregates feature matrix for each file using the beat frame numbers given in the files in beats_folder
+    using the provided agg_function and librosa.feature.sync. It is supposed that the feature matrix has been
+    calculated using sr=44100 and hop_size=1024/
+    :param features_folder:
+    :param beats_folder:
+    :param time_col: whether the 1st column of feature matrix contains time. Default: True.
+    :param agg_function: default: np.median
+    :param output_folder: default: features_folder + '-aggregated/'
+    :return:
+    '''
+    if not output_folder:
+        output_folder = features_folder.rstrip('/') + '-aggregated/'
+    for f in os.listdir(features_folder):
+        file_path = os.path.join(features_folder, f)
+        if os.path.isfile(file_path) and file_path.endswith('.csv'):
+            base_name = f[:f.index('_')]
+            beat_file_path = beats_folder + base_name + beat_file_suffix
+            print file_path
+            features_data = np.genfromtxt(file_path, delimiter=',').T
+            if time_col:
+                features_data = features_data[1:]
+            beat_frames = np.genfromtxt(beat_file_path, dtype=int)
+            feat_beats = lr.feature.sync(features_data, beat_frames, aggregate=agg_function)
+            np.savetxt(output_folder + base_name + '.csv', feat_beats.T, delimiter=',', fmt='%.8f')
+
+
+if __name__ == '__main__':
+    aggregate_vamp_features(default_features_folder, default_beats_folder)
--- code/seconds_to_frames.py
+++ code/seconds_to_frames.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-from aggregate import hop_size, sr
+from aggregate import hop_size, sr, beat_file_suffix
 
 default_input_folder = '../data/qm-tempotracker/'
 #default_input_folder = '../data/beatroot/'
@@ -9,6 +9,13 @@ default_output_folder = '../data/frames/'
 
 
 def seconds_to_frames(input_folder, output_folder):
+    '''
+    Converts a sequence of time values from the first column of the input file to a sequence of frame numbers
+     and saves this sequence of the ouptut file
+    :param input_folder:
+    :param output_folder:
+    :return:
+    '''
     seconds_per_frame = float(hop_size) / sr
     for f in os.listdir(input_folder):
         file_path = os.path.join(input_folder, f)
@@ -16,7 +23,7 @@ def seconds_to_frames(input_folder, output_folder):
             base_name = f[:f.index('_')]
             data = np.genfromtxt(file_path, usecols=(0), delimiter=',').T
             frames = [int(round(seconds / seconds_per_frame)) for seconds in data]
-            np.savetxt(os.path.join(output_folder, '%s_beats.csv' % base_name), frames, fmt='%d')
+            np.savetxt(os.path.join(output_folder, base_name + beat_file_suffix), frames, fmt='%d')
             print 'Done ' + f
 
 
